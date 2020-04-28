d209dbeaf95a4ca0b37fc3c3f13e8603d0b73079
Liam Wallace <45268305+Lwallace3@users.noreply.github.com>
2020-04-28 20:25:51 +0100

    Delete Driver.java

--- cs308-bostonsubway/src/driver/Driver.java
+++
@@ -1,8 +0,0 @@
-package driver;
-
-public class Driver {
-    public static void main(String[] args) {
-        ConsoleInterface console = new ConsoleInterface("bostonmetro.txt");
-        console.run();
-    }
-}
